71c903ad664361e28eef3520fcf71905eca86234
Kirill Mikulich <kirill.michenko@mail.ru>
2025-03-25 12:35:26 +0300

    scss modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'typescript-plugin-css-modules',
+    },
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Алиас для удобных импортов
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase', // camelCase-имена классов в JS (опционально)
+    },
+  },
+});
