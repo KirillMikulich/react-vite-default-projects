5f21ca6330b3ca9941747c103faf6f962adc35bc
Kirill Mikulich <kirill.michenko@mail.ru>
2025-03-25 15:08:04 +0300

    fix by convetions

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import ParentRoute from '@/routes/__root';
 import IndexRoute from '@/routes/main';
 import AboutRoute from './routes/about';
 import { createRouter } from '@tanstack/react-router';
-import PostsRoute from './routes/posts';
+import PostsRoute from './routes/posts/root';
 import PostsIdRoute from './routes/posts/$postId';
 
 const routeTree = ParentRoute.addChildren([IndexRoute, AboutRoute, PostsRoute, PostsIdRoute]);
